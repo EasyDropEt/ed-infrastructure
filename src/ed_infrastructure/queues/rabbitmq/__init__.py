b703fe6b96df5b590cab28dddcb3e01ab1b14c43
ffekirnew <ffekirnew0808@gmail.com>
2025-05-30 09:07:52 +0300

    chore: Remove unused queues

--- src/ed_infrastructure/queues/rabbitmq/__init__.py
+++
@@ -1,7 +0,0 @@
-from ed_infrastructure.queues.rabbitmq.producer import RabbitMQProducer
-from ed_infrastructure.queues.rabbitmq.subscriber import RabbitMQSubscriber
-
-__all__ = [
-    "RabbitMQProducer",
-    "RabbitMQSubscriber",
-]
