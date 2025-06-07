8938c187059c45c22798113a44051a0fbb4bec6d
ffekirnew <ffekirnew0808@gmail.com>
2025-06-07 15:08:31 +0300

    fix: Remove unused modules

--- src/ed_infrastructure/persistence/interfaces/__init__.py
+++
@@ -1,5 +0,0 @@
-from ed_infrastructure.persistence.interfaces.abc_async_db_client import \
-    ABCAsyncDbClient
-from ed_infrastructure.persistence.interfaces.abc_db_client import ABCDbClient
-
-__all__ = ["ABCDbClient", "ABCAsyncDbClient"]
--- src/ed_infrastructure/persistence/interfaces/abc_async_db_client.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class ABCAsyncDbClient(metaclass=ABCMeta):
-    @abstractmethod
-    async def start(self): ...
-
-    @abstractmethod
-    async def stop(self): ...
